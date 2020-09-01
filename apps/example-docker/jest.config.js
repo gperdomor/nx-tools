16802c20289e4bbbab1acc45bd98ca8e180ccb77
Gustavo Perdomo <gperdomor@gmail.com>
2020-08-31 21:37:41 -0400

    feat: update to nx 10.1.0

--- apps/example-docker/jest.config.js
+++ apps/example-docker/jest.config.js
@@ -2,4 +2,5 @@ module.exports = {
   name: 'example-app',
   preset: '../../jest.config.js',
   coverageDirectory: '../../coverage/apps/example-app',
+  globals: { 'ts-jest': { tsConfig: '<rootDir>/tsconfig.spec.json' } },
 };
