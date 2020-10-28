57fc9fa2c4004cb2fda44772500925f11d6c27cc
Gustavo Perdomo <gperdomor@gmail.com>
2020-10-28 13:03:49 -0300

    chore: update to nx 10.3.1

--- apps/example-prisma/jest.config.js
+++ apps/example-prisma/jest.config.js
@@ -1,10 +1,10 @@
 module.exports = {
-  name: 'example-prisma',
-  preset: '../../jest.config.js',
+  preset: '../../jest.preset.js',
   globals: {
     'ts-jest': {
       tsConfig: '<rootDir>/tsconfig.spec.json',
     },
   },
   coverageDirectory: '../../coverage/apps/example-prisma',
+  displayName: 'example-prisma',
 };
