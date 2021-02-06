0814050fc494319941bf7b32eaecc55578a6fcab
wSedlacek <wsedlacekc@gmail.com>
2021-02-06 11:07:41 -0700

    refactor(example-prisma): fix after repo resturcture
    Signed-off-by: wSedlacek <wsedlacekc@gmail.com>

---
+++ packages/example-prisma/data/migrations/20210206175145_nx_prisma/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "email" TEXT NOT NULL,
+    "name" TEXT
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User.email_unique" ON "User"("email");
