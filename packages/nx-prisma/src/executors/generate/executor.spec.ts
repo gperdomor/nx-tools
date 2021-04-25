be045caf3a01b96b84e296e0bb7fa561d8e9433b
Gustavo Perdomo <gperdomor@gmail.com>
2021-04-24 20:00:32 -0400

    test: update timeout for prisma tests

--- packages/nx-prisma/src/executors/generate/executor.spec.ts
+++ packages/nx-prisma/src/executors/generate/executor.spec.ts
@@ -11,5 +11,5 @@ export const generateSuite = () =>
       const output = await executor(options);
       expect(output.stderr).toBeFalsy();
       expect(output.success).toBeTruthy();
-    }, 10000);
+    }, 20000);
   });
--- packages/nx-prisma/src/executors/migrations/executor.spec.ts
+++ packages/nx-prisma/src/executors/migrations/executor.spec.ts
@@ -11,5 +11,5 @@ export const migrationsSuite = () =>
       const output = await executor(options);
       expect(output.stderr).toBeFalsy();
       expect(output.success).toBeTruthy();
-    }, 10000);
+    }, 20000);
   });
