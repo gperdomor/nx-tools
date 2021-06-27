6696ed9d8ce45bac8da4a131f67d793da4faeaa1
Gustavo Perdomo <gperdomor@gmail.com>
2021-06-27 01:37:21 -0400

    chore: bump versions

--- publish.sh
+++ publish.sh
@@ -1,5 +1,5 @@
-npm publish --access public --tag next dist/packages/ci-context
-npm publish --access public --tag next dist/packages/core
-npm publish --access public --tag next dist/packages/docker-meta
-npm publish --access public --tag next dist/packages/nx-docker
-npm publish --access public --tag next dist/packages/nx-prisma
+npm publish --access public dist/packages/ci-context
+npm publish --access public dist/packages/core
+npm publish --access public dist/packages/docker-meta
+npm publish --access public dist/packages/nx-docker
+npm publish --access public dist/packages/nx-prisma
