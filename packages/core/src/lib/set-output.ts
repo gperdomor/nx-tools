e077d50fcc7a5ba936d31503c637386bb0180912
Gustavo Perdomo <gperdomor@gmail.com>
2021-09-12 23:39:05 -0300

    feat: update nx stack to 12.9.0

--- packages/core/src/lib/set-output.ts
+++ packages/core/src/lib/set-output.ts
@@ -1,5 +1,5 @@
 import { issueCommand } from '@actions/core/lib/command';
 
-export function setOutput(name: string, value: any): void {
+export function setOutput(name: string, value: unknown): void {
   issueCommand('set-output', { name }, value);
 }
