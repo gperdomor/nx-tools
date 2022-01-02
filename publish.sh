41207505aa09a8b0eb5a0ab73705d393fc082d2f
Gustavo Perdomo <gperdomor@gmail.com>
2022-01-02 14:10:15 -0400

    feat(core): switch from swc to tsc again

--- publish.sh
+++ publish.sh
@@ -1,5 +1,5 @@
 npm publish --access public dist/packages/ci-context
 npm publish --access public dist/packages/core
-npm publish --access public dist/packages/docker-meta
+npm publish --access public dist/packages/docker-metadata
 npm publish --access public dist/packages/nx-docker
 npm publish --access public dist/packages/nx-prisma
