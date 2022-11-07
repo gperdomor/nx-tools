168a7983160ae451750474dd42a28cb17a83d961
Gustavo Perdomo <gperdomor@gmail.com>
2022-11-07 16:00:41 -0300

    feat(nx-container): added init generator (#639)

---
+++ packages/nx-container/src/generators/init/generator.ts
@@ -0,0 +1,54 @@
+import {
+  formatFiles,
+  generateFiles,
+  ProjectConfiguration,
+  readNxJson,
+  readProjectConfiguration,
+  Tree,
+  updateProjectConfiguration,
+} from '@nrwl/devkit';
+import * as path from 'path';
+import { InitGeneratorSchema } from './schema';
+
+function addFiles(tree: Tree, project: ProjectConfiguration, template) {
+  const templateOptions = {
+    projectName: project.name,
+    template: '',
+  };
+  generateFiles(tree, path.join(__dirname, 'files', template || 'empty'), project.root, templateOptions);
+}
+
+export default async function (tree: Tree, options: InitGeneratorSchema) {
+  const project = readProjectConfiguration(tree, options.project);
+  const nx = readNxJson();
+
+  updateProjectConfiguration(tree, options.project, {
+    ...project,
+    targets: {
+      ...project.targets,
+      container: {
+        executor: '@nx-tools/nx-container:build',
+        options: {
+          engine: options.engine,
+          metadata: {
+            images: [`${nx.npmScope}/${project.name}`],
+            load: true,
+            tags: [
+              'type=schedule',
+              'type=ref,event=branch',
+              'type=ref,event=tag',
+              'type=ref,event=pr',
+              'type=sha,prefix=sha-',
+            ],
+          },
+        },
+      },
+    },
+  });
+
+  addFiles(tree, project, options.template);
+
+  if (!options.skipFormat) {
+    await formatFiles(tree);
+  }
+}
--- publish.sh
+++ publish.sh
@@ -2,4 +2,4 @@ npm publish --access public dist/packages/core --tag next
 npm publish --access public dist/packages/ci-context --tag next
 npm publish --access public dist/packages/container-metadata --tag next
 npm publish --access public dist/packages/nx-container --tag next
-# npm publish --access public dist/packages/nx-prisma --tag next
+npm publish --access public dist/packages/nx-prisma --tag next
