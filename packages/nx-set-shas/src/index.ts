d90da6be4303d37c1aa199f9c421be0a34823b16
Gustavo Perdomo <gperdomor@gmail.com>
2022-11-09 10:30:15 -0300

    feat(nx-set-shas): move from oclif to commander

--- packages/nx-set-shas/src/index.ts
+++ packages/nx-set-shas/src/index.ts
@@ -1,3 +1,27 @@
-#!/usr/bin/env node
+#! /usr/bin/env node
 
-export { run } from '@oclif/core';
+import { Command } from 'commander';
+import { defaultWorkingDirectory, gitlab } from './lib/commands/gitlab';
+
+const program = new Command();
+
+program
+  .command('gitlab')
+  .description('Find latest successful pipeline of gitlab project')
+  .option('-t, --token <token>', 'authentication token')
+  .option('-o, --output <output>')
+  .option('-d, --working-dir <directory>', 'The directory where your repository is located', defaultWorkingDirectory)
+  .option(
+    '--error-on-no-successful-pipeline <error>',
+    'By default, if no successful pipeline is found on the main branch to determine the SHA, we will log a warning and use HEAD~1. Enable this option to error and exit instead.',
+    false
+  )
+  .requiredOption(
+    '-b, --branch <branch>',
+    'The name of the main branch in your repo, used as the target of PRs. E.g. main, master etc',
+    process.env['CI_DEFAULT_BRANCH']
+  )
+  .requiredOption('-p, --project <project>', 'The ID of the project.', process.env['CI_PROJECT_ID'])
+  .action(gitlab);
+
+program.parse(process.argv);
---
+++ packages/nx-set-shas/src/lib/commands/gitlab.ts
@@ -0,0 +1,134 @@
+import { green, red, yellow } from 'colorette';
+import { execSync } from 'node:child_process';
+import { existsSync, readFileSync, writeFileSync } from 'node:fs';
+import { fetch } from 'undici';
+import { findExistingCommit } from '../utils/git-utils';
+
+export const defaultWorkingDirectory = '.';
+
+export const gitlab = async (options: any) => {
+  const { branch, project, token, errorOnNoSuccessfulPipeline, output, workingDir } = options;
+
+  if (workingDir !== defaultWorkingDirectory) {
+    if (existsSync(workingDir)) {
+      process.chdir(workingDir);
+    } else {
+      process.stdout.write('\n');
+      process.stdout.write(yellow(`WARNING: Working directory '${workingDir}' doesn't exist.\n`));
+    }
+  }
+
+  let BASE_SHA;
+  const eventName = process.env['CI_MERGE_REQUEST_ID'] ? 'pull_request' : '';
+  let HEAD_SHA = execSync(`git rev-parse HEAD`, { encoding: 'utf-8' }).trim();
+
+  if (eventName === 'pull_request') {
+    BASE_SHA = execSync(`git merge-base origin/${branch} HEAD`, { encoding: 'utf-8' });
+  } else {
+    try {
+      BASE_SHA = await findSuccessfulCommit(project, branch, token);
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    } catch (e: any) {
+      process.stdout.write(red(e.message));
+      process.exit(1);
+    }
+
+    if (!BASE_SHA) {
+      if (errorOnNoSuccessfulPipeline) {
+        reportFailure(branch);
+        process.exit(1);
+      } else {
+        process.stdout.write('\n');
+        process.stdout.write(yellow(`WARNING: Unable to find a successful pipeline run on 'origin/${branch}'\n`));
+        process.stdout.write(yellow(`We are therefore defaulting to use HEAD~1 on 'origin/${branch}'\n`));
+        process.stdout.write('\n');
+        process.stdout.write(
+          yellow(
+            `NOTE: You can instead make this a hard error by setting flag 'error-on-no-successful-pipeline' on the running script.\n`
+          )
+        );
+
+        BASE_SHA = execSync(`git rev-parse origin/${branch}~1`, { encoding: 'utf-8' });
+      }
+    } else {
+      process.stdout.write('\n');
+      process.stdout.write(`Found the last successful pipeline run on 'origin/${branch}'\n`);
+      process.stdout.write(`Commit: ${BASE_SHA}\n\n`);
+    }
+  }
+
+  const stripNewLineEndings = (sha: string) => sha.replace('\n', '');
+  BASE_SHA = stripNewLineEndings(BASE_SHA);
+  HEAD_SHA = stripNewLineEndings(HEAD_SHA);
+  process.stdout.write(green(`NX_BASE: ${BASE_SHA}\n`));
+  process.stdout.write(green(`NX_HEAD: ${HEAD_SHA}\n`));
+
+  let lines: string[] = [];
+
+  if (output) {
+    if (existsSync(output)) {
+      const variables = readFileSync(output).toString('utf-8').split('\n');
+      lines = variables.filter(
+        (variable) => !(variable.startsWith('NX_BASE') || variable.startsWith('NX_HEAD') || variable === '')
+      );
+    }
+    lines.push(`NX_BASE=${BASE_SHA}`, `NX_HEAD=${HEAD_SHA}`);
+    writeFileSync(output, lines.join('\n'), { encoding: 'utf-8' });
+    process.stdout.write(green(`NX_BASE and NX_HEAD environment variables have been written to '${output}'`));
+  }
+};
+
+const reportFailure = (branchName: string) => {
+  process.stdout.write(
+    red(`
+      Unable to find a successful pipeline run on 'origin/${branchName}'
+      NOTE: You have set 'error-on-no-successful-pipeline' on the script so this is a hard error.
+      Is it possible that you have no runs currently on 'origin/${branchName}'?
+      - If yes, then you should run the pipeline without this flag first.
+      - If no, then you might have changed your git history and those commits no longer exist.`)
+  );
+};
+
+/**
+ * Find last successful pipeline run on the repo
+ * @param {number} project
+ * @param {string} branch
+ * @param {string} token
+ * @returns
+ */
+const findSuccessfulCommit = async (
+  project: number,
+  branch: string,
+  token: string | undefined
+): Promise<string | undefined> => {
+  const params: Record<string, string> = {
+    scope: 'finished',
+    status: 'success',
+    ref: branch,
+    per_page: '50',
+  };
+
+  const response = await fetch(
+    `https://gitlab.com/api/v4/projects/${project}/pipelines?${new URLSearchParams(params).toString()}`,
+    {
+      headers: token
+        ? {
+            Authorization: `Bearer ${token}`,
+          }
+        : undefined,
+    }
+  );
+
+  const json: any = await response.json();
+
+  let shas: string[];
+
+  if (response.ok) {
+    shas = json.map((pipeline: { sha: string }) => pipeline.sha);
+  } else {
+    process.stdout.write(red(json.message));
+    process.exit(1);
+  }
+
+  return findExistingCommit(shas);
+};
