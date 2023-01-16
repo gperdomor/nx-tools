52f7e1b70fe9d8e21e3eeac38b7878260151f057
Gustavo Perdomo <gperdomor@gmail.com>
2023-01-16 00:22:56 -0300

    fix(ci-context): fix missing drone, travis and semaphore initialization in repo factory

--- packages/ci-context/src/lib/repo.factory.ts
+++ packages/ci-context/src/lib/repo.factory.ts
@@ -2,11 +2,14 @@ import { RepoMetadata } from './interfaces';
 import * as azure from './utils/azure-devops';
 import * as bitbucket from './utils/bitbucket';
 import * as circle from './utils/circle';
+import * as drone from './utils/drone';
 import * as github from './utils/github';
 import * as gitlab from './utils/gitlab';
 import * as jenkins from './utils/jenkins';
-import * as teamcity from './utils/teamcity';
 import * as local from './utils/local';
+import * as semaphore from './utils/semaphore';
+import * as teamcity from './utils/teamcity';
+import * as travis from './utils/travis';
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const ci = require('ci-info');
@@ -25,6 +28,10 @@ export class RepoProxyFactory {
       return circle.repo();
     }
 
+    if (ci.DRONE) {
+      return drone.repo();
+    }
+
     if (ci.GITHUB_ACTIONS) {
       return github.repo(token);
     }
@@ -35,6 +42,14 @@ export class RepoProxyFactory {
 
     if (ci.JENKINS) {
       return jenkins.repo();
+    }
+
+    if (ci.SEMAPHORE) {
+      return semaphore.repo();
+    }
+
+    if (ci.TRAVIS) {
+      return travis.repo();
     }
 
     if (ci.TEAMCITY) {
