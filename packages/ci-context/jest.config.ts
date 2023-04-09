f740d4abba679514f72cedabf3f548ddfadba3ad
Gustavo Perdomo <gperdomor@gmail.com>
2023-04-09 16:30:32 -0400

    style(ci-context): format jest config file

--- packages/ci-context/jest.config.ts
+++ packages/ci-context/jest.config.ts
@@ -2,15 +2,9 @@
 export default {
   displayName: 'ci-context',
   preset: '../../jest.preset.js',
-  globals: {},
   testEnvironment: 'node',
   transform: {
-    '^.+\\.[tj]s$': [
-      'ts-jest',
-      {
-        tsconfig: '<rootDir>/tsconfig.spec.json',
-      },
-    ],
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
   coverageDirectory: '../../coverage/packages/ci-context',
