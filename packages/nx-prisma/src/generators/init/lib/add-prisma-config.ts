146e9243e2b6ca45e194a91662fb22af42b8240f
Gustavo Perdomo <gperdomor@gmail.com>
2023-08-16 18:07:13 -0400

    feat(nx-prisma): initial support for prisma 5

--- packages/nx-prisma/src/generators/init/lib/add-prisma-config.ts
+++ packages/nx-prisma/src/generators/init/lib/add-prisma-config.ts
@@ -1,12 +1,7 @@
 import { generateFiles, joinPathFragments, ProjectConfiguration, stripIndents, Tree } from '@nx/devkit';
 import { InitGeneratorSchema } from '../schema';
 
-export function addPrismaConfig(
-  tree: Tree,
-  options: InitGeneratorSchema,
-  project: ProjectConfiguration,
-  prismaVersion: '3' | '4'
-): void {
+export function addPrismaConfig(tree: Tree, options: InitGeneratorSchema, project: ProjectConfiguration): void {
   if (tree.exists(joinPathFragments(project.root, 'prisma', 'schema.prisma'))) {
     throw new Error(
       stripIndents`The "schema.prisma" file already exists in the project "${options.project}". Are you sure this is the right project to set up Prisma?
@@ -14,7 +9,6 @@ export function addPrismaConfig(
     );
   }
 
-  // const filesDir = prismaVersion === '4' ? 'files/v4' : 'files/v3';
   const filesDir = 'files/v4';
 
   generateFiles(tree, joinPathFragments(__dirname, '..', filesDir), joinPathFragments(project.root, 'prisma'), {});
