d68c14132decdc23988f1f41c2154eac7e0add43
Gustavo Perdomo <gperdomor@gmail.com>
2024-04-21 16:45:17 -0400

    chore: update projects configurations to prepare for nx-release

--- publish.sh
+++
@@ -1,7 +0,0 @@
-npm publish --access public dist/packages/core --tag next
-npm publish --access public dist/packages/ci-context --tag next
-npm publish --access public dist/packages/container-metadata --tag next
-npm publish --access public dist/packages/nx-container --tag next
-npm publish --access public dist/packages/nx-prisma --tag next
-npm publish --access public dist/packages/nx-set-shas --tag next
-npm publish --access public dist/packages/nx-graphql-codegen --tag next
