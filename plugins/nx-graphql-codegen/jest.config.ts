e7b073f95bf1c3d185537039a0b77f1734ac65d3
Gustavo Perdomo <gperdomor@gmail.com>
2024-05-25 23:51:18 -0400

    feat(nx-graphql-codegen): move project to plugins directory

--- plugins/nx-graphql-codegen/jest.config.ts
+++ plugins/nx-graphql-codegen/jest.config.ts
@@ -6,5 +6,5 @@ export default {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/packages/nx-graphql-codegen',
+  coverageDirectory: '../../coverage/plugins/nx-graphql-codegen',
 };
