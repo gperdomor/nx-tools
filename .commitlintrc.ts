9be343956818024a36bc962ba9d6c70820303ff3
Gustavo Perdomo <gperdomor@gmail.com>
2024-10-01 20:27:35 -0300

    fix(deps): bump nrwl monorepo from 19.6.0 to v19.8.3 (#1137)
    * fix(deps): bump nrwl monorepo from 19.6.0 to v19.8.3

--- .commitlintrc.ts
+++ .commitlintrc.ts
@@ -3,6 +3,6 @@ import { utils } from '@commitlint/config-nx-scopes';
 module.exports = {
   extends: ['@commitlint/config-conventional', '@commitlint/config-nx-scopes'],
   rules: {
-    'scope-enum': async (ctx) => [2, 'always', ['release', ...(await utils.getProjects(ctx))]],
+    'scope-enum': async (ctx) => [2, 'always', ['deps', 'release', ...(await utils.getProjects(ctx))]],
   },
 };
