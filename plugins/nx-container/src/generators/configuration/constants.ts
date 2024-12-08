487c6d9c09580df2effa58032badb7dcc34b4705
Gustavo Perdomo <gperdomor@gmail.com>
2024-12-08 18:01:36 -0300

    feat(nx-container): improvements in inferred task (#1164)

--- plugins/nx-container/src/generators/configuration/constants.ts
+++ plugins/nx-container/src/generators/configuration/constants.ts
@@ -1,3 +1,4 @@
 export const DEFAULT_ENGINE = 'docker';
+export const DEFAULT_REGISTRY = 'docker.io';
 
 export const DEFAULT_TEMPLATE = 'empty';
--- plugins/nx-container/src/generators/init/schema.d.ts
+++ plugins/nx-container/src/generators/init/schema.d.ts
@@ -3,4 +3,5 @@ export interface Schema {
    * Provide the default container engine to be used.
    */
   defaultEngine?: 'docker' | 'podman' | 'kaniko';
+  defaultRegistry?: string;
 }
