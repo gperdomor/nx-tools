1e6785236883aa5f772ddb94fb31456d5176e4c4
Gustavo Perdomo <gperdomor@gmail.com>
2024-12-16 11:46:32 -0300

    test(nx-graphql-codegen): migrate tests from jest to vitest (#1174)

--- plugins/nx-graphql-codegen/jest.config.ts
+++
@@ -1,9 +0,0 @@
-export default {
-  displayName: 'nx-graphql-codegen',
-  preset: '../../jest.preset.js',
-  transform: {
-    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
-  },
-  moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: 'test-output/jest/coverage',
-};
