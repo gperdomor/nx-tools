7ad2017122bc22b333dd993eb8b393c6292da2a8
Gustavo Perdomo <gperdomor@gmail.com>
2025-05-17 22:55:48 -0400

    feat(container-metadata): updated config files to align with new library generators (#1233)

--- packages/container-metadata/vite.config.ts
+++ packages/container-metadata/vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
 
-export default defineConfig({
+export default defineConfig(() => ({
   root: __dirname,
   cacheDir: '../../node_modules/.vite/packages/container-metadata',
   plugins: [],
@@ -14,7 +14,9 @@ export default defineConfig({
     environment: 'node',
     include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
     reporters: ['default'],
-    coverage: { reportsDirectory: '../../coverage/packages/container-metadata', provider: 'v8' },
-    pool: 'forks',
+    coverage: {
+      reportsDirectory: './test-output/vitest/coverage',
+      provider: 'v8' as const,
+    },
   },
-});
+}));
