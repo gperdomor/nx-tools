1a02f95afd952d62ff5f124f87ecd188ddc81304
Gustavo Perdomo <gperdomor@gmail.com>
2025-06-16 22:43:03 -0400

    feat(website): initial nx-graphql-codegen docs (#1255)

--- plugins/nx-graphql-codegen/src/interfaces.ts
+++ plugins/nx-graphql-codegen/src/interfaces.ts
@@ -1,26 +1,31 @@
 export interface CodeGenBase {
   /**
    * Specifies the path to the desired graphql code generator config file to be processed instead of the default path. Both absolute and relative paths are supported.
+   * @default "codegen.ts"
    */
   config?: string;
 
   /**
-   * If true, the executor will not log any output to the console. Defaults to false.
+   * If true, the executor will not log any output to the console.
+   * @default false
    */
   silent?: boolean;
 
   /**
-   * If true, the executor will log additional information to the console. Defaults to false.
+   * If true, the executor will log additional information to the console.
+   * @default false
    */
   verbose?: boolean;
 
   /**
-   * If true, the executor will log debug information to the console. Defaults to false.
+   * If true, the executor will log debug information to the console.
+   * @default false
    */
   debug?: boolean;
 
   /**
-   * If true, the executor will only log errors to the console. Defaults to false.
+   * If true, the executor will only log errors to the console.
+   * @default false
    */
   errorsOnly?: boolean;
 }
