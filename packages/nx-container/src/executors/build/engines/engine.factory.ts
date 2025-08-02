e6bcecae7c6d4d9f41ea039ebbe425590be1aba6
Gustavo Perdomo <gperdomor@gmail.com>
2025-08-02 13:07:20 -0400

    feat(nx-container): remove kaniko suport
    kaniko engine is removed
    
    BREAKING CHANGE: kaniko engine is no longer supported

--- packages/nx-container/src/executors/build/engines/engine.factory.ts
+++ packages/nx-container/src/executors/build/engines/engine.factory.ts
@@ -1,6 +1,5 @@
 import { Docker } from './docker/docker.engine';
 import { EngineAdapter } from './engine-adapter';
-import { Kaniko } from './kaniko/kaniko.engine';
 import { Podman } from './podman/podman.engine';
 
 export class EngineFactory {
@@ -10,8 +9,6 @@ export class EngineFactory {
         return new Docker();
       case 'podman':
         return new Podman();
-      case 'kaniko':
-        return new Kaniko();
       default:
         throw new Error('Unsupported Container Engine');
     }
