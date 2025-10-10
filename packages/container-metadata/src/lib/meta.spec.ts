708bd38b17e27de8d9c5eee4a3474bf7867255a2
Gustavo Perdomo <gperdomor@gmail.com>
2025-10-10 15:13:27 -0300

    fix: fix tests

--- packages/container-metadata/src/lib/meta.spec.ts
+++ packages/container-metadata/src/lib/meta.spec.ts
@@ -4,7 +4,7 @@ import { workspaceRoot } from '@nx/devkit';
 import * as fs from 'node:fs';
 import * as path from 'node:path';
 import * as stdEnv from 'std-env';
-import repoFixture from '../../__tests__/fixtures/repo.json';
+import repoFixture from '../../__tests__/fixtures/repo.json' with { type: 'json' };
 import { mockConsole, stubEnvsFromFile } from '../test-utils.spec.js';
 import { Inputs, getContext, getInputs } from './context.js';
 import { Meta, Version } from './meta.js';
